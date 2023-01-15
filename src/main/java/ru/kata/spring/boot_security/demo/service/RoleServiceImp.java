0928e4a19a2407223f6aac47346767dd9a1c4a08
falcetnik <niektarii@yandex.ru>
2023-01-15 21:45:06 +0300

    load3

--- src/main/java/ru/kata/spring/boot_security/demo/service/RoleServiceImp.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/RoleServiceImp.java
@@ -1,10 +1,12 @@
 package ru.kata.spring.boot_security.demo.service;
 
+import org.springframework.stereotype.Service;
 import ru.kata.spring.boot_security.demo.model.Role;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Service
 public class RoleServiceImp implements RoleService {
 
     @Override
